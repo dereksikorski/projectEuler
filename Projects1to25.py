4373f1d3e44af9141e0f572a9d1ee1ce9fcc94cf
dmsikors <dmsikors@iu.edu>
2022-06-14 11:28:58 -0500

    Finished through 8

--- Projects1to25.py
+++ Projects1to25.py
@@ -1,5 +1,6 @@
 ## Imports:
 
+from multiprocessing import Condition
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -161,6 +162,90 @@ def Project5(n):
     print("-"*150)
     
 
+
+###########
+def Project6(n):
+    """
+    Given n, find difference between (n-(n-1) + n-(n-2) + ... + n)**2 and n**2 + ... + (n-(n-2))**2 + (n-(n-1))**2
+    """
+    print("-"*150)
+    print("\n### Project 6 ###\n")
+    print(f"--> Finding difference: (1+2+...{n})**2 -(1**2 +...+{n}**2) \n\n")
+    l1, l2 = [], [num for num in range (1,n+1)]
+    for m in range (1,n+1):
+        l1.append(m**2)
+    
+    ans = sum(l2)**2 - sum(l1)
+    print(f"Answer: {ans}")
+    print("-"*150)
+
+
+############
+def Project7(n):
+    """
+    Find the nth prime number
+    """
+
+    print("-"*150)
+    print("\n### Project 7 ###\n")
+    print(f"--> Finding {n}th prime \n\n")
+
+    prime_dict = {}
+    next_test = 1
+    while len(prime_dict.values()) < n:
+        next_test += 1
+        Condition = True
+        for i in range(2,next_test):
+            if next_test % i == 0:
+                Condition = False
+        
+        if Condition == True:
+            prime_dict[str(next_test)] = next_test
+
+    print(f"Answer: {next_test}")
+    print("-"*150)
+
+
+
+
+#############
+def Project8(n,m):
+    """
+    Find the n adjacent terms of m that have the largest product
+    --> n and m both (int)
+    """
+    print("-"*150)
+    print("\n### Project 8 ###\n")
+    print(f"--> Finding largest product of {n} consecutive numbers in {m} \n\n")
+
+    num_str = str(m)
+    max_product = 0
+    for i in range(0, len(num_str)-2):
+        current_product = 1
+        current_slice = num_str[i:i+n]
+        for num in current_slice:
+            current_product *= int(num)
+        if current_product > max_product:
+            max_product = current_product
+
+    print(f"Answer: {max_product}")
+    print("-"*150)
+
+
+
+
+
 if __name__ == "__main__":
 
-    Project5(20)
+    n = 4
+
+    m = 7316717653133062491922511967442657474235534919493496983520312774506326239578318016984801869478851843858615607891129494954595017379583319528532088055111254069874715852386305071569329096329522744304355766896648950445244523161731856403098711121722383113622298934233803081353362766142828064444866452387493035890729629049156044077239071381051585930796086670172427121883998797908792274921901699720888093776657273330010533678812202354218097512545405947522435258490771167055601360483958644670632441572215539753697817977846174064955149290862569321978468622482839722413756570560574902614079729686524145351004748216637048440319989000889524345065854122758866688116427171479924442928230863465674813919123162824586178664583591245665294765456828489128831426076900422421902267105562632111110937054421750694165896040807198403850962455444362981230987879927244284909188845801561660979191338754992005240636899125607176060588611646710940507754100225698315520005593572972571636269561882670428252483600823257530420752963450
+
+
+
+
+
+
+
+
+    Project8(13,m)
